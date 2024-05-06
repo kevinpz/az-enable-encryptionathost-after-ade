d987adddb612d36bad12461efce937863977329c
Kevin Printz <kevinprintz@microsoft.com>
2024-05-06 10:51:42 -0700

    fix bug

--- enable-encryptionathost.ps1
+++ enable-encryptionathost.ps1
@@ -3,6 +3,11 @@
 
 # Select to the subscription
 Set-AzContext -Subscription $subscriptionName | Out-Null
+
+# Get the VM
+Write-Host "Getting the VM"
+$vm = Get-AzVm -ResourceGroupName $rgName -Name $vmName
  
 # Enable Encryption at Host
-Update-AzVM -VM $vmName -ResourceGroupName $rgName -EncryptionAtHost $true
+Write-Host "-> Enabling encryption at host"
+Update-AzVM -VM $vm -ResourceGroupName $rgName -EncryptionAtHost $true
